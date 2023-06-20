31cf9b38dbe859d80dc06bb13dbd0cee28d34222
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-06-20 17:56:32 +0300

    add technology schema

--- services/schemas/technology.js
+++ services/schemas/technology.js
@@ -2,18 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const technology = new Schema({
+const technologySchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+    },
     name: {
         type: String,
         unique: true,
         required: [true, 'Technology name is required'],
     },
-    amount: {
-        type: Number,
-        unique: true,
-    },
+    amount: Number,
 });
 
-const Technology = mongoose.model('technology', technology);
+const Frontend = mongoose.model('technology', technologySchema, 'frontend');
+const Backend = mongoose.model('technology', technologySchema, 'backend');
+const QA = mongoose.model('technology', technologySchema, 'qa');
+const PM = mongoose.model('technology', technologySchema, 'pm');
+const Design = mongoose.model('technology', technologySchema, 'pm');
 
-module.exports = Technology;
+module.exports = { Frontend, Backend, QA, PM, Design };
